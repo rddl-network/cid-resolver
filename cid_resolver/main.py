b7262e8dad910a954f71bae9043072e281467cc0
Jürgen Eckel <juergen@riddleandcode.com>
2022-11-24 16:20:10 +0100

    added linting and blackified the python file
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- cid_resolver/main.py
+++ cid_resolver/main.py
@@ -6,30 +6,30 @@ app = FastAPI()
 import redis
 
 
-REDIS_HOST = config('REDIS_HOST','localhost')
-REDIS_PORT = config('REDIS_PORT',6379)
-REDIS_AUTH = config('REDIS_AUTH')
+REDIS_HOST = config("REDIS_HOST", "localhost")
+REDIS_PORT = config("REDIS_PORT", 6379)
+REDIS_AUTH = config("REDIS_AUTH")
 
 
-pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, password = REDIS_AUTH, db=0)
+pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_AUTH, db=0)
 redis_client = redis.Redis(connection_pool=pool)
 
+
 @app.get("/entry/cid")
-async def resolve_cid(cid:str):
+async def resolve_cid(cid: str):
     try:
         url = redis_client.get(cid)
         if not url:
             raise HTTPException(status_code=404, detail="Item not found.")
-        return { "cid": cid, "url": url}
+        return {"cid": cid, "url": url}
     except redis.exceptions.ConnectionError as e:
         raise HTTPException(status_code=420, detail="Connection to Redis server failed.")
 
+
 @app.post("/entry")
-async def register_cid( cid:str, url:str):
-    try: 
-        redis_client.set( cid, url)
+async def register_cid(cid: str, url: str):
+    try:
+        redis_client.set(cid, url)
         return {}
     except redis.exceptions.ConnectionError as e:
         raise HTTPException(status_code=420, detail="Connection to Redis server failed.")
-    
-    
